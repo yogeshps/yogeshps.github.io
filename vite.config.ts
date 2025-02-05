af12d8845847f71d8c222fab4bdcbfc350c455f6
Yogesh <40452854+yogeshps@users.noreply.github.com>
2025-02-06 01:38:25 +0800

    struggling to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      },
+    },
+  },
 })
